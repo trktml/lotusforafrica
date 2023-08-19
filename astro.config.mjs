2edfeefebf394a1aeb2623fa30f84815e0d6053d
Tarık <utariktemel@gmail.com>
2023-08-19 17:01:55 +0200

    added astro compress, purgecss, robots-txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,22 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import purgecss from 'astro-purgecss';
+import compress from 'astro-compress';
 
-import react from "@astrojs/react";
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+    site: 'https://lotusfoundationafrica.com/',
+    integrations: [
+        react(),
+        compress(),
+        robotsTxt(),
+        purgecss({
+            fontFace: true,
+            keyframes: true,
+            rejected: true,
+            variables: true,
+        }),
+    ],
 });
