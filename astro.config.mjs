ef1cd352306a665876b42a96e4e3b91b842012e2
Tarık <utariktemel@gmail.com>
2023-08-19 17:23:55 +0200

    critters that inlines critical CSS and lazy-loads

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,17 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import purgecss from 'astro-purgecss';
 import compress from 'astro-compress';
-
 import robotsTxt from 'astro-robots-txt';
+
+import critters from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://lotusfoundationafrica.com/',
-    integrations: [
-        react(),
-        compress(),
-        robotsTxt(),
-        purgecss({
-            fontFace: true,
-            keyframes: true,
-            rejected: true,
-            variables: true,
-        }),
-    ],
+  site: 'https://lotusfoundationafrica.com/',
+  integrations: [react(), compress(), robotsTxt(), purgecss({
+    fontFace: true,
+    keyframes: true,
+    rejected: true,
+    variables: true
+  }), critters()]
 });
