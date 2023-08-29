e5d7d2b08f3d083c67e9a092b85cd3c1a3f61b7e
Tarık <utariktemel@gmail.com>
2023-08-29 08:08:04 +0200

    undo purgecss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,15 +1,6 @@
-const purgecss = require('@fullhuman/postcss-purgecss');
 module.exports = {
     plugins: [
         require('autoprefixer'),
-        purgecss({
-            content: ['./src/**/*.{astro,tsx,js,html}'],
-            safelist: {
-                standard: [/.*slider-wrapper.*/],
-                deep: [/.*slider-wrapper.*/],
-                greedy: [/.*slider-wrapper.*/],
-            },
-        }),
         require('cssnano'),
     ],
 };
