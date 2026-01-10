0391242b6e57b257adc0bc7fc84f894ee4c8eaf5
Tarık <utariktemel@gmail.com>
2026-01-10 16:56:43 +0100

    new wells added

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -9,9 +9,9 @@ import '../scss/components/slider.scss';
 import well1 from '../assets/img/slider/001-well.webp';
 import well2 from '../assets/img/slider/050-well.webp';
 import well3 from '../assets/img/slider/075-well.webp';
-import well4 from '../assets/img/slider/147-well.webp';
-import well5 from '../assets/img/slider/148-well.webp';
-import well6 from '../assets/img/slider/149-well.webp';
+import well4 from '../assets/img/slider/155-well.webp';
+import well5 from '../assets/img/slider/156-well.webp';
+import well6 from '../assets/img/slider/157-well.webp';
 
 export default function Slider(params: AwesomeSliderProps) {
     return (
@@ -22,9 +22,9 @@ export default function Slider(params: AwesomeSliderProps) {
             bullets={false}
             animation="openAnimation"
         >
-            <div data-alt="slider 149. well image" data-src={well6.src} />
-            <div data-alt="slider 148. well image" data-src={well5.src} />
-            <div data-alt="slider 147. well image" data-src={well4.src} />
+            <div data-alt="slider 157. well image" data-src={well6.src} />
+            <div data-alt="slider 156. well image" data-src={well5.src} />
+            <div data-alt="slider 155. well image" data-src={well4.src} />
             <div data-alt="slider 75. well image" data-src={well3.src} />
             <div data-alt="slider 50. well image" data-src={well2.src} />
             <div data-alt="slider 1. well image" data-src={well1.src} />
