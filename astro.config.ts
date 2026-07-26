dedb540398d098f269a17c2d344df9d816653493
trktml <utariktemel@gmail.com>
2026-07-26 06:39:03 +0200

    fix(css): disable svgo in cssnano and silence sass deprecation warnings

--- astro.config.ts
+++ astro.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
             preprocessorOptions: {
                 scss: {
                     quietDeps: true,
+                    silenceDeprecations: ['import', 'legacy-js-api'],
                 },
             },
         },
