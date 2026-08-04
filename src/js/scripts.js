a850faa0be0c95bf84e2548587f89273eb206267
trktml <46635827+trktml@users.noreply.github.com>
2026-08-04 00:23:25 +0000

    Refactor `navbarShrink` to use `const` instead of `var` in `scripts.js`

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     // Navbar shrink function
-    var navbarShrink = function () {
+    const navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
         if (!navbarCollapsible) {
             return;
